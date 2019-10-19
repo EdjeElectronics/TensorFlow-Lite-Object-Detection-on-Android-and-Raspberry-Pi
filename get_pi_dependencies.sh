a17825f8d74300e734f76e33cd09a5231a48a803
Evan <evan.juras@gmail.com>
2019-10-19 01:16:27 -0600

    Update get_pi_dependencies.sh

--- get_pi_dependencies.sh
+++ get_pi_dependencies.sh
@@ -1,3 +1,13 @@
-  #!/bin/bash
-  
-  It's a shell script baby!!
+#!/bin/bash
+
+# Get packages required for OpenCV
+
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install qt4-dev-tools libatlas-base-dev
+
+sudo pip3 install opencv-python
+
+# Get packages required for TensorFlow Lite
+sudo pip3 install numpy
