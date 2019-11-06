176fee8aef4f5796dbbd761a0cd3c15c29ac2163
Evan <evan.juras@gmail.com>
2019-11-06 01:20:44 -0700

    Add BGR to RGB conversion

--- TFLite_detection_video.py
+++ TFLite_detection_video.py
@@ -94,7 +94,8 @@ while(video.isOpened()):
 
     # Acquire frame and resize to expected shape [1xHxWx3]
     ret, frame = video.read()
-    frame_resized = cv2.resize(frame, (width, height))
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (width, height))
     input_data = np.expand_dims(frame_resized, axis=0)
 
     # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
