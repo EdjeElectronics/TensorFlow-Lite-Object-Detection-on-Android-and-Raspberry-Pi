51480cbae3da4503dd8531b001faff204f321fcc
Evan <evan.juras@gmail.com>
2019-11-05 19:47:18 -0700

    Oh yeah I forgot TF v1.13 doesn't work.

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -11,5 +11,23 @@ pip3 install opencv-python
 
 # Get packages required for TensorFlow
 
-pip3 install tensorflow
+# For now, downloading the TensorFlow builds from lhelontra's "TensorFlow on ARM" repository
+# Thanks lhelontra for being super awesome!
+# Will change to just 'pip3 install tensorflow' once newer versions of TF are added to piwheels
+
+#pip3 install tensorflow
+
+version=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+
+if [ $version == "3.7" ]; then
+wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.0.0/tensorflow-2.0.0-cp37-none-linux_armv7l.whl
+pip3 install tensorflow-2.0.0-cp37-none-linux_armv7l.whl
+rm tensorflow-2.0.0-cp37-none-linux_armv7l.whl
+fi
+
+if [ $version == "3.5" ]; then
+wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.14.0/tensorflow-1.14.0-cp35-none-linux_armv7l.whl
+pip3 install tensorflow-1.14.0-cp35-none-linux_armv7l.whl
+rm tensorflow-1.14.0-cp35-none-linux_armv7l.whl
+fi
 
