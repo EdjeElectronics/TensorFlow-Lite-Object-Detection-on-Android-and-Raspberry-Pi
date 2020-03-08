1ee487e9e6a53fcd786886f0bc693c298e4da3fb
Evan <evan.juras@gmail.com>
2020-03-08 14:32:45 -0600

    Use tflite_runtime packages

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -7,12 +7,11 @@ sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get -y install libxvidcore-dev libx264-dev
 sudo apt-get -y install qt4-dev-tools libatlas-base-dev
 
+# Need to get an older version of OpenCV because version 4 has errors
 pip3 install opencv-python==3.4.6.27
 
 # Get packages required for TensorFlow
-
-# For now, downloading the TensorFlow builds from lhelontra's "TensorFlow on ARM" repository
-# Thanks lhelontra for being super awesome!
+# Using the tflite_runtime packages available at https://www.tensorflow.org/lite/guide/python
 # Will change to just 'pip3 install tensorflow' once newer versions of TF are added to piwheels
 
 #pip3 install tensorflow
@@ -20,14 +19,10 @@ pip3 install opencv-python==3.4.6.27
 version=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
 
 if [ $version == "3.7" ]; then
-wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.0.0/tensorflow-2.0.0-cp37-none-linux_armv7l.whl
-pip3 install tensorflow-2.0.0-cp37-none-linux_armv7l.whl
-rm tensorflow-2.0.0-cp37-none-linux_armv7l.whl
+pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl
 fi
 
 if [ $version == "3.5" ]; then
-wget https://dl.google.com/coral/python/tflite_runtime-1.14.0-cp35-cp35m-linux_armv7l.whl
-pip3 install tflite_runtime-1.14.0-cp35-cp35m-linux_armv7l.whl
-rm tflite_runtime-1.14.0-cp35-cp35m-linux_armv7l.whl
+pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp35-cp35m-linux_armv7l.whl
 fi
 
