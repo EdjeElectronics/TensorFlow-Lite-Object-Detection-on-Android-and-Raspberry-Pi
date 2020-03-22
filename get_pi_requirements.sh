d21b92bb6ffc0a2bc0913f4e9f54ed34aedbc768
Jinto Lonappan <10410815+jintolonappan@users.noreply.github.com>
2020-03-21 18:39:37 -0700

    Updated "version=$(python" to "python3"
    Updated "version=$(python" to "python3" as python would currently return 2.7 and none of the "if" condition would get satisfied for TFLite installation.
    Also added several '"echo" commands in order to show progress to user.

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Get packages required for OpenCV
-
+echo "!!! Installing dependencies for OpenCV !!!"
 sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get -y install libxvidcore-dev libx264-dev
 sudo apt-get -y install qt4-dev-tools libatlas-base-dev
 
 # Need to get an older version of OpenCV because version 4 has errors
+echo "!!! Installing OpenCV version 3.4.6.27 !!!"
 pip3 install opencv-python==3.4.6.27
 
 # Get packages required for TensorFlow
@@ -16,13 +17,18 @@ pip3 install opencv-python==3.4.6.27
 
 #pip3 install tensorflow
 
-version=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+version=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+
+echo "!!! Using Python version " $version " !!!"
 
 if [ $version == "3.7" ]; then
+echo "!!! Installing TFLite for Python 3.7 !!!"
 pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl
-fi
-
-if [ $version == "3.5" ]; then
+elif [ $version == "3.5" ]; then
+echo "!!! Installing TFLite for Python 3.5 !!!"
 pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp35-cp35m-linux_armv7l.whl
+else
+echo "!!! Suitable version of python not installed. Please install Python 3.5 or 3.7 !!!" 
 fi
 
+echo "!!! Installation complete !!!"
