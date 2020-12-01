7ddda1e00ce42adaca603442ad65617b2df5fe3f
Rembert Muylle <74431619+Rembie01@users.noreply.github.com>
2020-12-01 21:24:19 +0100

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,10 @@
+class Detect:
+    """Reads the x and y coordinates from TFLite_detection_webcam.py"""
+    def __init__(self):
+        pass
+
+    def x_coord(self):
+        pass
+
+    def y_coord(self):
+        pass
---
+++ movement.py
@@ -0,0 +1,16 @@
+class Movement:
+    """Allows the stepper motor to move"""
+    def __init__(self):
+        pass
+
+    def move_left(self):
+        pass
+
+    def move_right(self):
+        pass
+
+    def move_up(self):
+        pass
+
+    def move_down(self):
+        pass
