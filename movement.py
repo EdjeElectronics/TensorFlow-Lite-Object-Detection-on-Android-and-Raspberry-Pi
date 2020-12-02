2c7b7ae1855b65fe754637f046159465e7664fa6
Rembert Muylle <74431619+Rembie01@users.noreply.github.com>
2020-12-02 17:05:48 +0100

    Update movement.py

--- movement.py
+++ movement.py
@@ -1,16 +1,25 @@
 class Movement:
     """Allows the stepper motor to move"""
-    def __init__(self):
-        pass
+    def __init__(self, tilt, rotate, rpm):
+        self.tilt = tilt
+        self.rotate = rotate
+        if rpm >= 0:
+            self.rpm = rpm
+        else:
+            self.rpm = -rpm
 
     def move_left(self):
-        pass
+
+        return 't:00000r:' + rotate + 's:' + rpm
 
     def move_right(self):
-        pass
+
+        return 't:00000r:' + rotate + 's:' + rpm
 
     def move_up(self):
-        pass
+
+        return 't:' + tilt + 'r:00000s:' + rpm
 
     def move_down(self):
-        pass
+
+        return 't:' + tilt + 'r:00000s:' + rpm
