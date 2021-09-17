1af3c01f052d8cfafbdf09e73a4465d8f9d20270
Rafa Almada <slimkaki@pop-os.localdomain>
2021-09-17 14:43:26 -0300

    adding -y flag

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Update
 sudo apt update
-sudo apt dist-upgrade
+sudo apt dist-upgrade -y
 
 # Run the pi requirements
 chmod +x get_pi_requirements.sh
@@ -17,7 +17,7 @@ sudo rm coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install libedgetpu1-std
+sudo apt-get install libedgetpu1-std -y
 
 wget https://dl.google.com/coral/canned_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite
 mv mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite Sample_TFLite_model/edgetpu.tflite
