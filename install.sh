0029cfe40b43b5d9704850f108146e637913289c
Rafa Almada <slimkaki@pop-os.localdomain>
2021-09-17 14:29:48 -0300

    Install script
    Co-authored-by: fontes99<pvfontes1000@gmail.com>

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update
+sudo apt update
+sudo apt dist-upgrade
+
+# Run the pi requirements
+chmod +x get_pi_requirements.sh
+sudo ./get_pi_requirements.sh
+
+# Get the tflite of mobilenet coco
+wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
+unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip -d Sample_TFLite_model
+sudo rm coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
+
+# Setting up the Coral TPU
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install libedgetpu1-std
+
+wget https://dl.google.com/coral/canned_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite
+mv mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite Sample_TFLite_model/edgetpu.tflite
+
+# Done!
