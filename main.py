2a9e8b7cdacd79932a554a9d62d4459b61fadd3c
slimkaki <rafaalmada98@gmail.com>
2021-09-21 15:20:57 -0300

    add more buttons, get categories, argument parser and GPIO pins image pin_layout.svg

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
-import subprocess, os, signal, time
+import subprocess, os, signal, time, argparse
 from gpiozero import Button
 
-def main():
+def main(MODEL_NAME, RESOLUTION, USE_EDGETPU, CATEGORIES):
     """Function that orchestrates the whole system"""
     # Start running safari mode
-    # safari()
+    # safariProcess = safari()
 
     # Conect button on GPIO2 and Ground
-    but = Button(2)
+    # Watch out for connenctions in 'pin_layout.svg'
+    query_button = Button(2)
     while (True):
-        if but.is_pressed:
+        if query_button.is_pressed:
             # Enter Query Mode
             print("Entering query mode!")
+            up_button = Button(3) # GPIO3 -> Up button
+            down_button = Button(4) # GPIO4 -> Down Button
+            # Kills safari mode
+            # os.killpg(os.getpgid(safariProcess.pid), signal.SIGTERM)
+
+            # Starts tts
+
         else:
             print("Button was not pressed...")
         time.sleep(1)
         
+def getAllCategories(dir):
+    """Needs testing!"""
+    """Function that get all available categories from model '.name' file"""
+    for root, dir, files in os.walk(dir):
+        for f in files:
+            if ".name" in f:
+                filename = f
+                break
+    cat = []
+    f = open(filename, "r")
+    for line in f.readlines():
+        cat.append(line)
+    return cat
 
-
-def safari():
+def safari(modeldir, resolution, edgetpu):
+    # Needs testing!
     p = subprocess.Popen(f"sudo python3 TFLite_detection_webcam --modeldir=Sample_TFLite_model --edgetpu", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-    
-    os.killpg(os.getpgid(p.pid), signal.SIGTERM)
     # output, err = p.communicate()
     # output.decode().split()[0]
+    return p
     
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--modeldir', help='Folder the .tflite file is located in', 
+                        default="Sample_TFLite_model/")
+    parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
+                        default='1280x720')
+    parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
+                        action='store_true')
+    args = parser.parse_args()
+
+    MODEL_NAME = args.modeldir
+    RESOLUTION = args.resolution
+    USE_EDGETPU = args.edgetpu
+    categories = getAllCategories(MODEL_NAME)
+    main(MODEL_NAME, RESOLUTION, USE_EDGETPU, categories)
