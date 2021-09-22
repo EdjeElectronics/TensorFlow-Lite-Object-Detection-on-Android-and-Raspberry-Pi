dcfb8d1e72153fa3220f6931a6280f88e11174b8
slimkaki <rafaalmada98@gmail.com>
2021-09-22 15:25:30 -0300

    Re-organizing main.py in a single class and integrating buttons with tts (started programming query mode)

--- main.py
+++ main.py
@@ -1,50 +1,103 @@
+from logging import captureWarnings
 import subprocess, os, signal, time, argparse
-from gpiozero import Button
+# from gpiozero import Button
+from gtts import gTTS
+from playsound import playsound
 
-def main(MODEL_NAME, RESOLUTION, USE_EDGETPU, CATEGORIES):
-    """Function that orchestrates the whole system"""
-    # Start running safari mode
-    # safariProcess = safari()
+class VMobi:
+    """Class that represents the system as a whole"""
 
-    # Conect button on GPIO2 and Ground
-    # Watch out for connenctions in 'pin_layout.svg'
-    query_button = Button(2)
-    while (True):
-        if query_button.is_pressed:
-            # Enter Query Mode
-            print("Entering query mode!")
-            up_button = Button(3) # GPIO3 -> Up button
-            down_button = Button(4) # GPIO4 -> Down Button
-            # Kills safari mode
-            # os.killpg(os.getpgid(safariProcess.pid), signal.SIGTERM)
+    def __init__(self, args, lang = "en"):
+        self.MODEL_DIR = args.modeldir # Directory of the .tflite file and names file
+        self.RESOLUTION = args.resolution # Camera resolution as in pixels 
+        self.USE_EDGETPU = args.edgetpu # Flag to use the google coral tpu
+        self.lang = lang # Language used on tts speech voice
 
-            # Starts tts
+        # self.main()
+        # self.test()
 
-        else:
-            print("Button was not pressed...")
+    def test(self):
+        categories = self.getAllCategories()
+        print(categories)
         time.sleep(1)
+        print("Now playing the audio")
         
-def getAllCategories(dir):
-    """Needs testing!"""
-    """Function that get all available categories from model '.name' file"""
-    for root, dir, files in os.walk(dir):
-        for f in files:
-            if ".name" in f:
-                filename = f
-                break
-    cat = []
-    f = open(filename, "r")
-    for line in f.readlines():
-        cat.append(line)
-    return cat
+        for cat in categories:
+            self.playVoice(cat)
 
-def safari(modeldir, resolution, edgetpu):
-    # Needs testing!
-    p = subprocess.Popen(f"sudo python3 TFLite_detection_webcam --modeldir=Sample_TFLite_model --edgetpu", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-    # output, err = p.communicate()
-    # output.decode().split()[0]
-    return p
-    
+    def main(self):
+        """Main function that orchestrates the product"""
+
+        # Get a list of the categories as strings
+        self.categories = self.getAllCategories(self.MODEL_DIR)
+
+        # Running the safari mode to run on the background
+        safari_proccess = self.safari()
+
+        # Conect button on GPIO2 and Ground
+        # Watch out for connenctions in 'pin_layout.svg'
+        self.query_button = Button(2)
+        while (True):
+            if self.query_button.is_pressed:
+                # Enter Query Mode
+                print("Entering query mode!")
+                self.queryMode_type1()
+                # Kills safari mode
+                # os.killpg(os.getpgid(safariProcess.pid), signal.SIGTERM)
+    #     time.sleep(1)
+
+    def safari(self):
+        """Function that start the MobileNet SSD v2 detection -- Safari Mode"""
+        # Needs testing!
+        if (self.USE_EDGETPU):
+            p = subprocess.Popen(f"sudo python3 TFLite_detection_webcam --modeldir={self.MODEL_DIR} --resolution={self.RESOLUTION} --edgetpu", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+        else:
+            p = subprocess.Popen(f"sudo python3 TFLite_detection_webcam --modeldir={self.MODEL_DIR} --resolution={self.RESOLUTION}", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+        
+        # If get the output is needed, do as below:
+        # output, err = p.communicate()
+        # output.decode().split()[0] # This will, be a formatted string of the output
+        return p
+
+    def queryMode_type1(self, categories):
+        """Query mode that functions only with buttons"""
+        # up_button = Button(3) # GPIO3 -> Up button
+        # down_button = Button(4) # GPIO4 -> Down Button
+
+        self.playVoice("Query mode activaded. Which category do you want?")
+
+        for cat in categories:
+            self.playVoice(cat)
+            if self.query_button.is_pressed:
+                selection = cat
+                break
+            time.sleep(0.5)
+
+        self.playVoice(f"Looking for {selection}.")
+        # Start looking for a specific selected item
+
+    def getAllCategories(self):
+        """Function that get all available categories from model '.name' file"""
+        for root, dir, files in os.walk(self.MODEL_DIR):
+            for f in files:
+                if ".name" in f:
+                    filename = f
+                    break
+        cat = []
+
+        f = open(self.MODEL_DIR + filename, "r")
+        for line in f.readlines():
+            cat.append(line.replace("\n", ""))
+        return cat
+
+    def playVoice(self, mText):
+        """Function used to play the string 'mText' in audio using tts"""
+        myobj = gTTS(text=mText, lang=self.lang, slow=False)
+
+        myobj.save("voice.mp3")
+        playsound("voice.mp3")
+        os.remove("voice.mp3")
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -56,8 +109,4 @@ if __name__ == '__main__':
                         action='store_true')
     args = parser.parse_args()
 
-    MODEL_NAME = args.modeldir
-    RESOLUTION = args.resolution
-    USE_EDGETPU = args.edgetpu
-    categories = getAllCategories(MODEL_NAME)
-    main(MODEL_NAME, RESOLUTION, USE_EDGETPU, categories)
+    helper = VMobi(args)
