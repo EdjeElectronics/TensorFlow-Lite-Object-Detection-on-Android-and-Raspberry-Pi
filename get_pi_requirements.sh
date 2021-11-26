00b008cbb58df3e540c342d396ca15459d790b85
Evan <evan.juras@gmail.com>
2021-11-26 11:32:02 -0700

    Update to work with newer Raspberry Pi OS release

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -5,10 +5,11 @@
 sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get -y install libxvidcore-dev libx264-dev
-sudo apt-get -y install qt4-dev-tools libatlas-base-dev
+sudo apt-get -y install qt4-dev-tools 
+sudo apt-get -y install libatlas-base-dev
 
 # Need to get an older version of OpenCV because version 4 has errors
-pip3 install opencv-python==3.4.6.27
+pip3 install opencv-python==3.4.11.41
 
 # Get packages required for TensorFlow
 # Using the tflite_runtime packages available at https://www.tensorflow.org/lite/guide/python
@@ -18,11 +19,22 @@ pip3 install opencv-python==3.4.6.27
 
 version=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
 
+if [ $version == "3.9" ]; then
+pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp39-cp39-linux_armv7l.whl
+fi
+
+if [ $version == "3.8" ]; then
+pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp38-cp38-linux_armv7l.whl
+fi
+
 if [ $version == "3.7" ]; then
-pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
+pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp37-cp37m-linux_armv7l.whl
+fi
+
+if [ $version == "3.6" ]; then
+pip3 install https://github.com/google-coral/pycoral/releases/download/v2.0.0/tflite_runtime-2.5.0.post1-cp36-cp36m-linux_armv7l.whl
 fi
 
 if [ $version == "3.5" ]; then
 pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp35-cp35m-linux_armv7l.whl
 fi
-
