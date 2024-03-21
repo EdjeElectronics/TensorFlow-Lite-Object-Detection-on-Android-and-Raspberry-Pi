4d4950414fe14ee0e85759e205d8cb86a76c690d
MkWf <markbwassef@gmail.com>
2024-03-20 20:42:40 -0400

    Adds Dagger-Hilt injection dependency

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.hilt.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Android/build.gradle.kts
+++ Android/build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.hiltAndroid) apply false
 }
