afcf4f0dee1a36b65da390ed325590b7084480a2
MkWf <markbwassef@gmail.com>
2024-03-22 08:22:46 -0400

    Create Resource.kt

---
+++ Android/app/src/main/java/io/ejtech/tflite/ui/detection/Resource.kt
@@ -0,0 +1,7 @@
+package io.ejtech.tflite.ui.detection
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+}
