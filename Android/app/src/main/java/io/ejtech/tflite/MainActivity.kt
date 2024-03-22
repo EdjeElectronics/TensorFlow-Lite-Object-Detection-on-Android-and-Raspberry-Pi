9a11a7319f8976edfe032eb288a2e28a4632b9cc
MkWf <markbwassef@gmail.com>
2024-03-22 16:06:02 -0400

    MainActivity and ViewModel
    Generates viewmodel for use in DetectionScreen

--- Android/app/src/main/java/io/ejtech/tflite/MainActivity.kt
+++ Android/app/src/main/java/io/ejtech/tflite/MainActivity.kt
@@ -1,26 +1,47 @@
 package io.ejtech.tflite
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import io.ejtech.tflite.ui.detection.DetectionScreen
+import io.ejtech.tflite.ui.detection.DetectionViewModel
 import io.ejtech.tflite.ui.theme.MyApplicationTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        keepScreenOn()
         setContent {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val detectionViewModel = hiltViewModel<DetectionViewModel>()
+                    DetectionScreen(
+                        detectionViewModel = detectionViewModel,
+                        detectionState = detectionViewModel.detectionState.value
+                    )
                 }
             }
         }
+    }
+
+    private fun keepScreenOn() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
--- Android/app/src/main/java/io/ejtech/tflite/ui/detection/DetectionViewModel.kt
+++ Android/app/src/main/java/io/ejtech/tflite/ui/detection/DetectionViewModel.kt
@@ -1,4 +1,68 @@
 package io.ejtech.tflite.ui.detection
 
-class DetectionViewModel() {
+import android.graphics.Bitmap
+import android.graphics.RectF
+import androidx.camera.core.ImageProxy
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class DetectionViewModel @Inject constructor(
+    private val objectDetectorHelper: ObjectDetectorHelper
+): ViewModel() {
+
+    private var _detectionState = mutableStateOf(DetectionState())
+    val detectionState: State<DetectionState> = _detectionState
+
+    init {
+        if(!detectionState.value.tensorflowEnabled){
+            viewModelScope.launch(Dispatchers.IO) {
+                objectDetectorHelper.initialize().collect { detectionData: Resource<DetectionState> ->
+                    when(detectionData){
+                        is Resource.Success -> {
+                            detectionData.data?.let{
+                                withContext(Dispatchers.Main){
+                                    _detectionState.value = _detectionState.value.copy(
+                                        tensorflowEnabled = it.tensorflowEnabled
+                                    )
+                                }
+                            }
+                        }
+                        is Resource.Loading -> {}
+                        is Resource.Error -> {
+                            detectionData.data?.let{
+                                withContext(Dispatchers.Main){
+                                    _detectionState.value = _detectionState.value.copy(
+                                        tensorflowEnabled = it.tensorflowEnabled
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun detectObjects(image: ImageProxy, bitmapBuffer: Bitmap) {
+        image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+
+        val imageRotation = image.imageInfo.rotationDegrees
+        val resultState = objectDetectorHelper.detect(bitmapBuffer, imageRotation)
+
+        viewModelScope.launch(Dispatchers.Main){
+            _detectionState.value = _detectionState.value.copy(
+                tensorflowDetections = resultState.tensorflowDetections,
+                tensorflowImageHeight = resultState.tensorflowImageHeight,
+                tensorflowImageWidth = resultState.tensorflowImageWidth
+            )
+        }
+    }
 }
