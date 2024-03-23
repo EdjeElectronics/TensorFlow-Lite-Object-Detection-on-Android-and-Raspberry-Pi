d5eac6795e529429350d6934b39eeda472b10e53
MkWf <markbwassef@gmail.com>
2024-03-23 14:27:37 -0400

    Squashed commit of the following:
    commit a174a70aa567992a05b7dd95773ea54b3ecbb538
    Author: MkWf <markbwassef@gmail.com>
    Date:   Sat Mar 23 14:27:00 2024 -0400
    
        Update DetectionScreen.kt
    
        Displays detection bounding boxes and labels.
    
    commit 53ab529bb0eab1055e215cc4e7b47695b5874cf5
    Author: MkWf <markbwassef@gmail.com>
    Date:   Fri Mar 22 17:49:26 2024 -0400
    
        Works ok but Y is off
    
    commit 339278464b8a151f780b96ae4af117214151ac00
    Author: MkWf <markbwassef@gmail.com>
    Date:   Fri Mar 22 17:45:00 2024 -0400
    
        Update DetectionScreen.kt

--- Android/app/src/main/java/io/ejtech/tflite/ui/detection/DetectionScreen.kt
+++ Android/app/src/main/java/io/ejtech/tflite/ui/detection/DetectionScreen.kt
@@ -4,7 +4,10 @@ import android.Manifest
 import android.app.Activity
 import android.content.pm.ActivityInfo
 import android.graphics.Bitmap
+import android.graphics.Paint
 import android.graphics.Paint.Align
+import android.graphics.Rect
+import android.graphics.RectF
 import android.view.OrientationEventListener
 import android.view.Surface
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -41,7 +44,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -51,14 +53,18 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextAlign
+
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.toSize
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.toRect
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import java.util.concurrent.ExecutorService
@@ -73,11 +79,9 @@ fun DetectionScreen(
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     val activity = (LocalContext.current as Activity)
-    val configuration = LocalConfiguration.current
     activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
     var cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
-    // Shutdown executor when screen is disposed of
     DisposableEffect(lifecycleOwner) {
         val observer = LifecycleEventObserver { _, event ->
             if (event == Lifecycle.Event.ON_DESTROY) {
@@ -91,42 +95,6 @@ fun DetectionScreen(
     }
 
     var bitmapBuffer: Bitmap? = null
-    var imageCapture: ImageCapture = remember {
-        ImageCapture.Builder()
-            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-            .build()
-    }
-
-    val degreeRotationState = remember { mutableStateOf(0f) }
-    val rotationState by animateFloatAsState(
-        targetValue = degreeRotationState.value, label = ""
-    )
-
-    var orientationEventListener = remember { mutableStateOf(object: OrientationEventListener(context){
-        override fun onOrientationChanged(orientation: Int) {
-            if ((orientation < 35 || orientation > 325)) { // PORTRAIT
-                imageCapture.targetRotation = Surface.ROTATION_0
-                degreeRotationState.value = 270f
-            } else if (orientation in 146..214) { // REVERSE PORTRAIT
-                imageCapture.targetRotation = Surface.ROTATION_180
-                degreeRotationState.value = 90f
-            } else if (orientation in 56..124) { // REVERSE LANDSCAPE
-                imageCapture.targetRotation = Surface.ROTATION_270
-                degreeRotationState.value = 180f
-            } else if (orientation in 236..304) { //LANDSCAPE
-                imageCapture.targetRotation = Surface.ROTATION_90
-                degreeRotationState.value = 0f
-            }
-        }
-    }) }
-    //Hack to prevent crash from too many listeners being enabled when rotating the phone too many times
-    //Disable before each enable
-    var orientationListenerEnabled = remember{ mutableStateOf(false) }
-    if(!orientationListenerEnabled.value) {
-        orientationListenerEnabled.value = true
-        orientationEventListener.value.enable()
-    }
-
     val camera = remember { mutableStateOf<Camera?>(null) }
     val cameraProviderFuture = remember {
         ProcessCameraProvider.getInstance(context)
@@ -135,7 +103,7 @@ fun DetectionScreen(
     val permissionsLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(),
         onResult = { granted ->
-            //hasWriteExternalStoragePermission = granted
+
         }
     )
     LaunchedEffect(key1 = true) {
@@ -146,19 +114,18 @@ fun DetectionScreen(
         )
     }
 
-    var boxsize by remember { mutableStateOf(IntSize.Zero)}
+    val textMeasurer = rememberTextMeasurer()
+
     Box(
         modifier = Modifier
             .fillMaxSize()
     ){
         AndroidView(
             modifier = Modifier
-                .fillMaxSize()
-                .onGloballyPositioned { coordinates ->
-                    boxsize = coordinates.size
-                },
+                .fillMaxSize(),
             factory = { context ->
                 PreviewView(context).also{
+                    it.scaleType = PreviewView.ScaleType.FILL_START
                     val preview = Preview.Builder()
                         .setTargetAspectRatio(AspectRatio.RATIO_4_3)
                         .build()
@@ -175,8 +142,6 @@ fun DetectionScreen(
                         .also{
                             it.setAnalyzer(cameraExecutor) { image ->
                                 if (bitmapBuffer == null) {
-                                    // The image rotation and RGB image buffer are initialized only once
-                                    // the analyzer has started running
                                     bitmapBuffer = Bitmap.createBitmap(
                                         image.width,
                                         image.height,
@@ -190,7 +155,6 @@ fun DetectionScreen(
                         }
 
                     try{
-                        //Need to unbind when we return from preview or it will rebind the camera and screen will remain black
                         cameraProviderFuture.get().unbindAll()
                         camera.value = cameraProviderFuture.get().bindToLifecycle(
                             lifecycleOwner,
@@ -205,57 +169,61 @@ fun DetectionScreen(
             }
         )
 
-        if(detectionState.tensorflowEnabled && detectionState.tensorflowDetections.isNotEmpty()){
-            val imageWidth = detectionState.tensorflowImageWidth
-            val imageHeight = detectionState.tensorflowImageHeight
+        if(detectionState.tensorflowEnabled && detectionState.tensorflowDetections.isNotEmpty()) {
+            Canvas(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                val scaleFactor = max(size.width / detectionState.tensorflowImageWidth, size.height / detectionState.tensorflowImageHeight)
+                for (detection in detectionState.tensorflowDetections) {
+                    val boundingBox = detection.boundingBox
 
-            for (detection in detectionState.tensorflowDetections){
-                val boundingBox = detection.boundingBox
-                val widthScale = 2168f / imageWidth
-                val heightScale = 1008f / imageHeight
+                    val top = boundingBox.top * scaleFactor
+                    val left = boundingBox.left * scaleFactor
+                    val bottom = boundingBox.bottom * scaleFactor
+                    val right = boundingBox.right * scaleFactor
+                    val scaledRect = RectF(left, top, right, bottom)
 
-                val top = boundingBox.top * widthScale
-                val bottom = boundingBox.bottom * widthScale
-                val left = boundingBox.left * widthScale
-                val right = boundingBox.right * widthScale
+                    var label = detection.categories[0].label
 
-                var label = detection.categories[0].label
-
-                Canvas(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    val rect = Rect(left, top, right, bottom)
                     drawRect(
                         topLeft = Offset(left, top),
                         color = Color.Green,
-                        style = Stroke(width = 8f),
-                        size = rect.size)
-                }
-
-                /*
-                Column(
-                ){
-                    Text(
-                        modifier = Modifier
-                            .background(Color.Black),
-                        text = label + (boundingBox.width().dp+8.dp).toString() + (boundingBox.height().dp+8.dp).toString(),
-                        fontSize = 16.sp,
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White,
+                        style = Stroke(width = 3.dp.toPx()),
+                        size = Size(scaledRect.width(), scaledRect.height())
                     )
-                    Box(
-                        modifier = Modifier
-                            //.absoluteOffset(left.dp-4.dp, top.dp-4.dp)
-                            .offset(left.dp-4.dp, top.dp-4.dp)
-                            .background(Color.Transparent)
-                            .border(2.dp, Color.Black)
-                            .size(boundingBox.width().dp+8.dp, boundingBox.height().dp+8.dp)
-                    ) {}
-                }*/
+
+                    val textBackgroundPaint = Paint()
+                    textBackgroundPaint.textSize = 14.sp.toPx()
+                    textBackgroundPaint.getTextBounds(label, 0, label.length, scaledRect.toRect())
+                    val textWidth = scaledRect.width()
+                    val textHeight = scaledRect.height()
+
+                    drawRect(
+                        topLeft = Offset(x = left, y = top - 60),
+                        color = Color.Black,
+                        size = Size(
+                            width = textWidth,
+                            height = 50F
+                        ),
+                    )
+
+                    drawText(
+                        textMeasurer = textMeasurer,
+                        text = label,
+                        topLeft = Offset(x = left, y = top - 75),
+                        style = TextStyle(
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.ExtraBold
+                        ),
+                        size = Size(
+                            width = textWidth + 30f,
+                            height = textHeight + 30f
+                        )
+                    )
+                }
             }
         }
     }
-
 }
