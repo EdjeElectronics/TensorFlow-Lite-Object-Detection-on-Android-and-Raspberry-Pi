62d4eae0934524ad9ff4bef800f9323a9cd48bec
MkWf <markbwassef@gmail.com>
2024-03-27 20:45:35 -0400

    Commented Resource

--- Android/app/src/main/java/io/ejtech/tflite/ui/detection/Resource.kt
+++ Android/app/src/main/java/io/ejtech/tflite/ui/detection/Resource.kt
@@ -1,5 +1,8 @@
 package io.ejtech.tflite.ui.detection
 
+/**
+ * Used for passing state between the ViewModel and Screen
+ */
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
