11348b9933105a4e536fbe9e74c043bfb03ad8ba
MkWf <markbwassef@gmail.com>
2024-03-27 20:44:37 -0400

    Commented TfliteApplication

--- Android/app/src/main/java/io/ejtech/tflite/TfliteApplication.kt
+++ Android/app/src/main/java/io/ejtech/tflite/TfliteApplication.kt
@@ -3,6 +3,9 @@ package io.ejtech.tflite
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Boilerplate for Dagger-Hilt dependency injection
+ */
 @HiltAndroidApp
 class TfliteApplication : Application() {
 }
