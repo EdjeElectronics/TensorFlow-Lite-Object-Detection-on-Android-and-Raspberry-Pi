f28b0f00f518102e76841f0d3700e26efeca9fe3
Evan <evan.juras@gmail.com>
2024-10-08 17:20:56 -0600

    Create train_val_test_split_yolo.py

---
+++ util_scripts/train_val_test_split_yolo.py
@@ -0,0 +1,78 @@
+### Python script to split a labeled image dataset into Train, Validation, and Test folders. Modified to work for YOLO txt files.
+# Author: Evan Juras, EJ Technology Consultants
+# Date: 4/10/21
+
+# Randomly splits images to 80% train, 10% validation, and 10% test, and moves them to their respective folders. 
+# This script is intended to be used in the TFLite Object Detection Colab notebook here:
+# https://colab.research.google.com/github/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/blob/master/Train_TFLite2_Object_Detction_Model.ipynb
+
+from pathlib import Path
+import random
+import os
+import sys
+
+# Define paths to image folders
+image_path = '/content/images/all'
+train_path = '/content/images/train'
+val_path = '/content/images/validation'
+test_path = '/content/images/test'
+
+# Get list of all images
+jpeg_file_list = [path for path in Path(image_path).rglob('*.jpeg')]
+jpg_file_list = [path for path in Path(image_path).rglob('*.jpg')]
+png_file_list = [path for path in Path(image_path).rglob('*.png')]
+bmp_file_list = [path for path in Path(image_path).rglob('*.bmp')]
+
+if sys.platform == 'linux':
+    JPEG_file_list = [path for path in Path(image_path).rglob('*.JPEG')]
+    JPG_file_list = [path for path in Path(image_path).rglob('*.JPG')]
+    file_list = jpg_file_list + JPG_file_list + png_file_list + bmp_file_list + JPEG_file_list + jpeg_file_list
+else:
+    file_list = jpg_file_list + png_file_list + bmp_file_list + jpeg_file_list
+
+file_num = len(file_list)
+print('Total images: %d' % file_num)
+
+# Determine number of files to move to each folder
+train_percent = 0.8  # 80% of the files go to train
+val_percent = 0.1 # 10% go to validation
+test_percent = 0.1 # 10% go to test
+train_num = int(file_num*train_percent)
+val_num = int(file_num*val_percent)
+test_num = file_num - train_num - val_num
+print('Images moving to train: %d' % train_num)
+print('Images moving to validation: %d' % val_num)
+print('Images moving to test: %d' % test_num)
+
+# Select 80% of files randomly and move them to train folder
+for i in range(train_num):
+    move_me = random.choice(file_list)
+    fn = move_me.name
+    base_fn = move_me.stem
+    parent_path = move_me.parent
+    txt_fn = base_fn + '.txt'
+    os.rename(move_me, train_path+'/'+fn)
+    os.rename(os.path.join(parent_path,txt_fn),os.path.join(train_path,txt_fn))
+    file_list.remove(move_me)
+
+# Select 10% of remaining files and move them to validation folder
+for i in range(val_num):
+    move_me = random.choice(file_list)
+    fn = move_me.name
+    base_fn = move_me.stem
+    parent_path = move_me.parent
+    txt_fn = base_fn + '.txt'
+    os.rename(move_me, val_path+'/'+fn)
+    os.rename(os.path.join(parent_path,txt_fn),os.path.join(val_path,txt_fn))
+    file_list.remove(move_me)
+
+# Move remaining files to test folder
+for i in range(test_num):
+    move_me = random.choice(file_list)
+    fn = move_me.name
+    base_fn = move_me.stem
+    parent_path = move_me.parent
+    txt_fn = base_fn + '.txt'
+    os.rename(move_me, test_path+'/'+fn)
+    os.rename(os.path.join(parent_path,txt_fn),os.path.join(test_path,txt_fn))
+    file_list.remove(move_me)
